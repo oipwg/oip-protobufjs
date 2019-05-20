f193f7e467408988b2c5a415233b054e4cb20fdd
orpheus <ryanachacon@gmail.com>
2019-05-20 15:56:44 -0700

    return optional webfmtd object with serialized fields

--- src/builders/decodeDescriptor.js
+++ src/builders/decodeDescriptor.js
@@ -3,19 +3,47 @@ import descriptor from 'protobufjs/ext/descriptor'
 
 /**
  * decode descriptor.proto into a Protobuf.js Type instance
- * @param buffer - encoded file descriptor
- * @returns {Type}
+ * @param protoDescriptor - encoded file descriptor
+ * @param {Boolean} [forWeb=false] - return a serialized object with fields, types, an enums to make it easier for UI
+ * @returns {Object}
  */
-export default function decodeDescriptor (buffer) {
-  if (!Buffer.isBuffer(buffer)) {
+export default function decodeDescriptor (protoDescriptor, forWeb = false) {
+  if (!Buffer.isBuffer(protoDescriptor)) {
     let uint8 = new Uint8Array()
-    const len = protobuf.util.base64.decode(buffer, uint8, 0)
+    const len = protobuf.util.base64.decode(protoDescriptor, uint8, 0)
     uint8 = new Uint8Array(len)
-    protobuf.util.base64.decode(buffer, uint8, 0)
-    buffer = uint8
+    protobuf.util.base64.decode(protoDescriptor, uint8, 0)
+    protoDescriptor = uint8
   }
-  let decodedDescriptor = descriptor.FileDescriptorSet.decode(buffer)
+  let decodedDescriptor = descriptor.FileDescriptorSet.decode(protoDescriptor)
   const root = protobuf.Root.fromDescriptor(decodedDescriptor)
   const type = root.lookupType('P')
-  return type
+
+  let webFmt = { enums: {}, fields: {}}
+
+  const nestedArray = type.nestedArray
+  for (let field of nestedArray) {
+    if (field.name !== 'Txid') {
+     webFmt.enums[field.name] = {
+       values: field.values,
+       rule: field.rule
+     }
+    }
+  }
+  const fieldArray = type.fieldsArray
+  for (let field of fieldArray) {
+    webFmt.fields[field.name] = {
+      type: field.type,
+      rule: field.rule,
+      id: field.id,
+      repeated: field.repeated,
+      extend: field.extend,
+      required: field.required
+    }
+  }
+
+  return {
+    type,
+    webFmt
+  }
 }

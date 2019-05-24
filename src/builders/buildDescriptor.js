95e8e8d73b69fceac384fbd7a35c4a3aa984da76
orpheus <ryanachacon@gmail.com>
2019-05-24 16:38:25 -0700

    protobuf wants to fight me

--- src/builders/buildDescriptor.js
+++ src/builders/buildDescriptor.js
@@ -3,8 +3,11 @@ import descriptor from 'protobufjs/ext/descriptor'
 
 import toPascalCase from 'js-pascalcase'
 import { camelCase } from 'lodash'
+import { ProtoModules } from '../index'
+import path from "path"
 
 const descriptorPackage = 'oipProto.templates'
+// const Txid = ProtoModules.oipProto.Txid
 
 /**
  * @typedef {Object} fieldData
@@ -12,6 +15,7 @@ const descriptorPackage = 'oipProto.templates'
  * @param {string} fieldData.type - field type
  * @param {string} [fieldData.rule] - rule as according to proto3 standards (must be 'repeated' or `undefined`)
  * @param {array} [fieldData.values] - **only used for enum** enum values in an array
+ * @param {number} [fieldData.index] - optionally declare proto tag number
  */
 
 /**
@@ -38,10 +42,27 @@ export default function buildDescriptor (fieldData) {
       }
     }
 
-  const P = new protobuf.Type('P')
+  let pRoot = new protobuf.Root()
+  pRoot.filename = 'p.proto'
+  pRoot.define(descriptorPackage)
+
   const Txid = new protobuf.Type('Txid').add(
     new protobuf.Field('raw', 1, 'bytes')
   )
+
+  // let tRoot = new protobuf.Root()
+  // tRoot.filename = 'txid.proto'
+  // tRoot.define('oipProto')
+  // tRoot.add(Txid)
+
+  // protobuf.load(path.resolve('./', 'src', 'messages', 'oipProto', 'txid.proto'))
+
+  // let descriptorSet = new protobuf.Root()
+  // descriptorSet.add(pRoot)
+  // descriptorSet.add(tRoot)
+
+  const P = new protobuf.Type('P')
+
   let txidMessageAdded = false
   const ENUM = 'enum'
   const OIP_REF = 'oipref'
@@ -66,25 +87,29 @@ export default function buildDescriptor (fieldData) {
     const type = field.type
 
     const lowercaseType = field.type.toLowerCase()
+    const tag = field.index || index
     switch (lowercaseType) {
       case ENUM:
         P.add(new protobuf.Enum(toPascalCase(name), enumValues))
         break
       case OIP_REF:
-        P.add(new protobuf.Field(name, index, 'Txid', rule))
         if (!txidMessageAdded) {
-          P.add(Txid)
+          pRoot.add(Txid)
           txidMessageAdded = true
         }
+        // pRoot.add(Txid)
+        let field = new protobuf.Field(name, tag, 'Txid', rule)
+        P.add(field)
+        // pRoot.remove(Txid)
         break
       default:
-        P.add(new protobuf.Field(name, index, type, rule))
+        P.add(new protobuf.Field(name, tag, type, rule))
     }
     counter += 1
   }
-  let root = new protobuf.Root()
-  root.define(descriptorPackage).add(P)
-  let descriptorFromRoot = root.toDescriptor('proto3')
+
+  pRoot.add(P)
+  let descriptorFromRoot = pRoot.toDescriptor('proto3')
   let buffer = descriptor.FileDescriptorSet.encode(descriptorFromRoot).finish()
 
   return buffer

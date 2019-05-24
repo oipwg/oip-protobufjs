a99924fb6168ce3e67c8e0f4f26667eb90a1d1a6
orpheus <ryanachacon@gmail.com>
2019-05-24 16:37:56 -0700

    Create buildTxids.js

---
+++ src/builders/buildTxids.js
@@ -0,0 +1,30 @@
+import { ProtoModules } from '../index'
+
+const Txid = ProtoModules.oipProto.Txid
+
+/**
+ * Build txid proto message
+ * @param {string|Array.<string>} txids - a string txid or an array of string txids
+ */
+export default function buildTxids (txids) {
+  if (Array.isArray(txids)) {
+    let newArray = []
+    for (let txid of txids) {
+      newArray.push(createTxid(txid))
+    }
+    return newArray
+  } else {
+    return createTxid(txids)
+  }
+}
+
+function createTxid (txid) {
+  const payload = {
+    raw: Buffer.from(txid, 'hex')
+  }
+  let err = Txid.verify(payload)
+  if (err) throw Error(`txid failed payload verification: ${err}`)
+
+  return Txid.create(payload)
+}
+

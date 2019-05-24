160d2cd7c66718aed920411fb5b70fe14e82a187
orpheus <ryanachacon@gmail.com>
2019-05-24 16:37:50 -0700

    convert types when building details

--- src/builders/buildOipDetails.js
+++ src/builders/buildOipDetails.js
@@ -1,18 +1,71 @@
 import decodeDescriptor from './decodeDescriptor'
 import { ProtoModules } from '../index'
+import buildTxids from './buildTxids'
 
 const ANY = ProtoModules.google.protobuf.Any
 const OipDetails = ProtoModules.oipProto.OipDetails
 
 const googleApis = 'type.googleapis.com/'
-function serializeNameToTypeUrl(name, path) {
+
+function serializeNameToTypeUrl (name, path) {
   let splitPath = path.split('.')
   if (splitPath[0] === '') {
     splitPath = splitPath.slice(1)
   }
-  splitPath[splitPath.length-1] = name
+  splitPath[splitPath.length - 1] = name
   splitPath = splitPath.join('.')
   return `${googleApis}${splitPath}`
+}
+
+const protoNumberFields = [
+  'double',
+  'float',
+  'int32',
+  'int64',
+  'uint32',
+  'uint64',
+  'sint32',
+  'sint64',
+  'fixed32',
+  'fixed64',
+  'sfixed32',
+  'sfixed64'
+]
+
+function typeConvBool (values) {
+  if (Array.isArray(values)) {
+    let newArray = []
+    for (let field of values) {
+      newArray.push(field === 'true')
+    }
+    return newArray
+  } else {
+    return values === 'true'
+  }
+}
+
+function typeConvNumber (values) {
+  if (Array.isArray(values)) {
+    let newArray = []
+    for (let field of values) {
+      newArray.push(Number(field))
+    }
+    return newArray
+  } else {
+    return Number(values)
+  }
+}
+
+function typeConvBytes (values) {
+  if (Array.isArray(values)) {
+    let newArray = []
+    for (let field of values) {
+      newArray.push(Buffer.from(field))
+    }
+    return newArray
+  } else {
+    return Buffer.from(values)
+  }
 }
 
 /**
@@ -52,6 +105,29 @@ export default function buildOipDetails (data) {
     if (descriptor) {
       TemplateType = decodeDescriptor(descriptor).type
     } else TemplateType = type
+
+    // serialize to correct type
+    const fieldArray = TemplateType.fieldsArray
+    for (let field in payload) {
+      if (payload.hasOwnProperty(field)) {
+        for (let f of fieldArray) {
+          if (field === f.name) {
+            if (f.type === 'Txid') {
+              payload[field] = buildTxids(payload[field])
+            }
+            if (f.type === 'bytes') {
+              payload[field] = typeConvBytes(payload[field])
+            }
+            if (f.type === 'bool') {
+              payload[field] = typeConvBool(payload[field])
+            }
+            if (protoNumberFields.includes(f.type)) {
+              payload[field] = typeConvNumber(payload[field])
+            }
+          }
+        }
+      }
+    }
 
     let err = TemplateType.verify(payload)
     if (err) throw Error(`Failed payload verification for: ${name}: ${err}`)
